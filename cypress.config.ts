45d2d5f3930476d00c8160b3a3ab8341516df139
Nicklas Bekkevold <44287251+nicklasbekkevold@users.noreply.github.com>
2023-01-17 16:06:54 +0100

    Setup report configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,14 @@ import mochawesomeReporterPlugin from 'cypress-mochawesome-reporter/plugin';
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'public',
+    charts: true,
+    reportPageTitle: 'Cypres demo report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on) {
       return mochawesomeReporterPlugin(on);
